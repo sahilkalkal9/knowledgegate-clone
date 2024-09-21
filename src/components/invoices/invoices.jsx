697ea72797ccb418bb887fec68493c882e9ad51f
Sahil Kalkal <sahilkalkal108@gmail.com>
2024-09-21 15:35:02 +0530

    commit

--- src/components/invoices/invoices.jsx
+++ src/components/invoices/invoices.jsx
@@ -2,6 +2,7 @@ import { useCollectionData } from "react-firebase-hooks/firestore"
 import { auth, firestore } from "../../firebase"
 import "../../global.scss"
 import downloads from "./downloads.png"
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 
 
 function Invoices() {
@@ -9,6 +10,40 @@ function Invoices() {
 
     const invoicesRef = firestore.collection("users").doc(auth.currentUser?.uid).collection("invoices")
     const [invoices] = useCollectionData(invoicesRef)
+
+    const saveFileToDevice = async (fileName) => {
+        try {
+            // Get the Firebase Storage reference
+            const storage = getStorage();
+
+            // Create a storage reference to the file
+            const fileRef = ref(storage, fileName);
+
+            // Get the download URL for the file
+            const downloadURL = await getDownloadURL(fileRef);
+
+            // Fetch the file data
+            const response = await fetch(downloadURL, { mode: 'no-cors' });
+            const fileData = await response.blob();
+
+            // Create a temporary URL for the file data
+            const fileURL = URL.createObjectURL(fileData);
+
+            // Create a link element
+            const link = document.createElement('a');
+            link.href = fileURL;
+            link.download = fileName;
+
+            // Trigger the click event to initiate the download
+            link.click();
+
+            // Clean up the temporary URL
+            URL.revokeObjectURL(fileURL);
+        } catch (error) {
+            console.error(`Error saving file "${fileName}" to device:`, error);
+            // Handle any errors that occur during the download process
+        }
+    };
 
 
 
@@ -26,7 +61,7 @@ function Invoices() {
                             ? <p>No invoices</p>
                             : (
                                 invoices && invoices.map((i) => (
-                                    <div className="invoice">
+                                    <div onClick={() => { saveFileToDevice(i.fileSource) }} className="invoice">
                                         <p className="invoice-name">
                                             {i.name}
                                         </p>
